3c4092b534ebf19e021d83e7a439aac37660bf0d
Markus Dahms <mad@automagically.de>
2006-05-22 14:28:23 +0000

    * boilerplated

--- thumbnailer/gen_schema.sh
+++ thumbnailer/gen_schema.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+# $Id:$
+
+#	G3DViewer - 3D object viewer
+#
+#	Copyright (C) 2005, 2006  Markus Dahms <mad@automagically.de>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 schemafile=g3d-thumbnailer.schema
 
 > $schemafile
@@ -44,7 +64,4 @@ add_schema "image@x-lwo" "LWO"
 cat >> $schemafile <<EOD
 	</schemalist>
 </gconfschemafile>
-<!-- vim:set ft=xml: -->
 EOD
-
-# vim:set ft=sh:
